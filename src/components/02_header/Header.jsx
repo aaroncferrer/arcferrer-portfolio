db2a0a168266836fa33acf4a856640ca3fce17dc
arcferrer5 <arcferrer5@gmail.com>
2023-04-10 10:48:20 +0800

    Setup general styles & css vars

---
+++ src/components/02_header/Header.jsx
@@ -0,0 +1,11 @@
+import './header.css';
+
+function Header(){
+    return(
+        <header>
+            Header
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/03_about/About.jsx
@@ -0,0 +1,11 @@
+import './about.css';
+
+function About(){
+    return(
+        <section id='about'>
+            About
+        </section>
+    )
+}
+
+export default About;
