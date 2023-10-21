47a9f503b590cb31c77f590dc62c0559ec8abfe4
arcferrer5 <arcferrer5@gmail.com>
2023-10-21 09:33:03 +0800

    Add nutri tech and updated resume

--- src/components/02_header/HeaderSocials.jsx
+++ src/components/02_header/HeaderSocials.jsx
@@ -1,30 +1,52 @@
-import {FaGithub} from 'react-icons/fa';
-import {BsLinkedin, BsTwitter, BsInstagram} from 'react-icons/bs';
-import {motion} from 'framer-motion';
+import { FaGithub } from "react-icons/fa";
+import { BsLinkedin, BsTwitter, BsInstagram } from "react-icons/bs";
+import { motion } from "framer-motion";
 
-function HeaderSocials(){
-
-
-    return(
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ 
-                    duration: 0.4, 
-                    ease: "easeInOut",
-                    delay: 0.5
-                }}
+function HeaderSocials() {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        duration: 0.4,
+        ease: "easeInOut",
+        delay: 0.5,
+      }}
+    >
+      <div className="header__socials">
+        <a
+          href="https://www.github.com/aaroncferrer"
+          target="_blank"
+          rel="noreferrer"
         >
-            <div className="header__socials">
-                <a href="https://www.github.com/aaroncferrer" target="_blank" rel='noreferrer'><FaGithub /></a>
-                <a href="https://www.linkedin.com/in/anthony-renzo-ferrer" target="_blank" rel='noreferrer'><BsLinkedin /></a>
-                <a href="https://twitter.com/aaroncferrer" target="_blank" rel='noreferrer'><BsTwitter /></a>
-                <a href="https://www.instagram.com/aaroncferrer/" target="_blank" rel='noreferrer'><BsInstagram /></a>
-            </div>
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/anthony-renzo-ferrer"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://twitter.com/aaroncferrer"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsTwitter />
+        </a>
+        <a
+          href="https://www.instagram.com/aaroncferrer/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsInstagram />
+        </a>
+      </div>
 
-            {/* <small>arcferrer5@gmail.com</small> */}
-        </motion.div>
-    )
+      {/* <small>arcferrer5@gmail.com</small> */}
+    </motion.div>
+  );
 }
 
 export default HeaderSocials;
